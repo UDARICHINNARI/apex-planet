83a4de130d9f5381f3f2101957f53beae3132d14
UDARICHINNARI <155717797+UDARICHINNARI@users.noreply.github.com>
2024-09-06 13:57:27 +0530

    Create design.php

---
+++ design.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8>
+    <meta http-equiv =" X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>curd operations</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container my-5">
+        <a href="user.php" class="btn btn-primary text-dark">Add User </a>
+
+    </div>
+</body>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">slno</th>
+            <th scope="col">id</th>
+            <th scope="col">firstname</th>
+            <th scope="col">lastname</th>
+            <th scope="col">email</th>
+            <th scope="col">password</th>
+            <th scope="col">operations</th>
+
+        </tr>
+    </thead>
+    <tbody>
+       
+        <tr>
+            <th scope="row">1</th>
+            <td>21566</td>
+            <td>nikhitha</td>
+            <td>mamidala</td>
+            <td>nikhithamamidala@gmail.com</td>
+            <td>@123$</td>
+            <td>
+                <a href="update.php" class="btn btn-primary text-dark">Update </a> <a href="delete.php" class="btn btn-danger text-dark">Delete </a>
+
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>21567</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>jacob@gmail.com</td>
+            <td>$4825</td>
+            <td>
+                <a href="update.php" class="btn btn-primary text-dark">Update </a> <a href="delete.php" class="btn btn-danger text-dark">Delete </a>
+
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>21568</td>
+            <td>Larry</td>
+            <td>lilly</td>
+            <td>Otto@gmail.com</td>
+            <td>3267</td>
+            <td>
+                <a href="update.php" class="btn btn-primary text-dark">Update </a> <a href="delete.php" class="btn btn-danger text-dark">Delete </a>
+
+
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+</tbody>
+
+</table>
+
+</html>
